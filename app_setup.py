204493f233ce5294927c7fce2b18cbcf47b9afea
Marcus Bowman <miliarch.mb@gmail.com>
2019-03-11 23:04:32 -0700

    Name replacement is getting better

--- app_setup.py
+++ app_setup.py
@@ -3,8 +3,10 @@ import os
 import sys
 from pathlib import Path
 
+dist_app_name = 'flaskbp'
 
-def search_replace_within_file(path_obj, search_str, replace_str):
+
+def replace_within_file(path_obj, search_str, replace_str):
     with open(path_obj, 'r', encoding='utf8') as f:
         contents = f.read()
 
@@ -14,42 +16,70 @@ def search_replace_within_file(path_obj, search_str, replace_str):
         f.write(contents)
 
 
+def check_if_rename_needed(base_dir, app_name):
+    """ Compare parent and child directories to identify if a rename is
+    necessary
+    """
+    dirs = []
+    dirs.extend([d.stem for d in base_dir.glob(dist_app_name)])
+    dirs.extend([d.stem for d in base_dir.glob(app_name)])
+
+    if dist_app_name in dirs and app_name not in dirs:
+        return True
+    else:
+        return False
+
+
+def rename_app(app_name):
+    """ Rename ./flaskbp to ./<app_name> """
+    try:
+        os.rename(dist_app_name, app_name)
+    except OSError as err:
+        sys.exit(err)
+    except Exception as e:
+        print('{}\n{}'.format(type(e), e))
+
+
 def main():
     # Set explicit base directory
     base_dir = Path(__file__).resolve().parents[0]
 
-    # Identify application name (parent dir name) for use later
+    # Identify application name (parent dir name)
     app_name = base_dir.resolve().stem
 
-    # Check if default name (flaskbp) exists in base_dir
-    flaskbp_present = True if list(base_dir.glob('flaskbp')) != [] else False
+    # Check if rename is needed
+    rename_needed = check_if_rename_needed(base_dir, app_name)
 
-    # Check if parent dir name matches a child dir
-    app_name_present = True if list(base_dir.glob(app_name)) != [] else False
+    # Rename routine
+    if rename_needed:
+        rename_app(app_name)
 
-    # Flag work needed when flaskbp child path exists and mismatches parent dir
-    work_needed = True if flaskbp_present and not app_name_present else False
+        # Gather file list to act on
+        file_list = []
+        file_list.extend(list(base_dir.glob('*.py')))
+        file_list.extend(list(base_dir.glob('*/*.py')))
+        file_list.extend(list(base_dir.glob('containers/*/*')))
+        file_list.extend(list(base_dir.glob('docker-compose*')))
 
-    if not work_needed:
-        help_str = 'Nothing to do!\n'
-        help_str += '\tflaskbp_present: {}\n'.format(flaskbp_present)
-        help_str += '\tapp_name_present: {}'.format(app_name_present)
-        print(help_str)
-        sys.exit(2)
+        for f in file_list:
+            if __file__[2:] not in str(f):
+                da_str = 'from {} import'.format(dist_app_name)
+                an_str = 'from {} import'.format(app_name)
+                replace_within_file(f, da_str, an_str)
 
-    # Rename flaskbp dir to app_name
-    os.rename('flaskbp', app_name)
+                trailing = ['.', ':', '_', '/']
+                for i in trailing:
+                    da_str = '{}{}'.format(dist_app_name, i)
+                    an_str = '{}{}'.format(app_name, i)
+                    replace_within_file(f, da_str, an_str)
 
-    # Gather file list to act on
-    file_list = []
-    file_list.extend(list(base_dir.glob('*.py')))
-    file_list.extend(list(base_dir.glob('*/*.py')))
-    file_list.extend(list(base_dir.glob('containers/*/*')))
-    file_list.extend(list(base_dir.glob('docker-compose*')))
+                leading = ['/', 'COPY ']
+                for i in leading:
+                    da_str = '{}{}'.format(i, dist_app_name)
+                    an_str = '{}{}'.format(i, app_name)
+                    replace_within_file(f, da_str, an_str)
 
-    for f in file_list:
-        if __file__[2:] not in str(f):
-            search_replace_within_file(f, 'flaskbp', app_name)
+
 
 
 if __name__ == '__main__':
