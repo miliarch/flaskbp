06c6f66d67f4454fd76670e3e638f42276176c13
Marcus Bowman <miliarch.mb@gmail.com>
2019-03-13 22:41:29 -0700

    Another improvement to name replacement

--- app_setup.py
+++ app_setup.py
@@ -54,6 +54,11 @@ def main():
     if rename_needed:
         rename_app(app_name)
 
+        # Define files to exclude (exact file name match)
+        blacklist_files = [
+            Path(__file__).name
+        ]
+
         # Gather file list to act on
         file_list = []
         file_list.extend(list(base_dir.glob('*.py')))
@@ -61,18 +66,26 @@ def main():
         file_list.extend(list(base_dir.glob('containers/*/*')))
         file_list.extend(list(base_dir.glob('docker-compose*')))
 
+        # Replace references to dist_app_name with app_name in files
         for f in file_list:
-            if __file__[2:] not in str(f):
+            if f.name not in blacklist_files:
+                # Imports
                 da_str = 'from {} import'.format(dist_app_name)
                 an_str = 'from {} import'.format(app_name)
                 replace_within_file(f, da_str, an_str)
 
+                da_str = 'import {}'.format(dist_app_name)
+                an_str = 'import {}'.format(app_name)
+                replace_within_file(f, da_str, an_str)
+
+                # Common trailing patterns
                 trailing = ['.', ':', '_', '/']
                 for i in trailing:
                     da_str = '{}{}'.format(dist_app_name, i)
                     an_str = '{}{}'.format(app_name, i)
                     replace_within_file(f, da_str, an_str)
 
+                # Common leading patterns
                 leading = ['/', 'COPY ']
                 for i in leading:
                     da_str = '{}{}'.format(i, dist_app_name)
