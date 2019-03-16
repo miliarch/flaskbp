30768beaf8c090cecaa577ed4da06e68956169e0
Marcus Bowman <miliarch.mb@gmail.com>
2019-03-16 15:02:21 -0700

    Converted f strings back to .format strings (better python version compatibility)

--- app_setup_dev.py
+++ app_setup_dev.py
@@ -38,7 +38,7 @@ def rename_app(old_path, new_path):
     except OSError as err:
         sys.exit(err)
     except Exception as e:
-        sys.exit(f'{type(e)}\n{e}')
+        sys.exit('{}\n{}'.format(type(e), e))
 
 
 def replace_within_file(file_path, search_str, replace_str):
@@ -63,13 +63,13 @@ def main():
     app_name = base_dir.resolve().name
 
     # Check if rename is needed
-    old_path = Path(f'{base_dir}/{dist_app_name}')
-    new_path = Path(f'{base_dir}/{app_name}')
+    old_path = Path('{}/{}'.format(base_dir, dist_app_name))
+    new_path = Path('{}/{}'.format(base_dir, app_name))
     rename_needed = check_if_rename_needed(base_dir, old_path, new_path)
 
     # Rename routine
     if rename_needed:
-        status_str = f'Renaming {old_path} to {new_path}\n'
+        status_str = 'Renaming {} to {}\n'.format(old_path, new_path)
         print(status_str)
         rename_app(old_path, new_path)
 
@@ -86,33 +86,34 @@ def main():
         file_list.extend(list(base_dir.glob('docker-compose*')))
 
         # Replace references to dist_app_name with app_name in files
-        status_str = f'Replacing references to {dist_app_name} in files:'
+        status_str = 'Replacing references to {} in files:'.format(
+            dist_app_name)
         print(status_str)
         for f in file_list:
             if f.name not in blacklist_files:
-                status_str = f'  - {f.resolve()}'
+                status_str = '  - {}'.format(f.resolve())
                 print(status_str)
                 # Imports
-                dan_str = f'from {dist_app_name} import'
-                an_str = f'from {app_name} import'
+                dan_str = 'from {} import'.format(dist_app_name)
+                an_str = 'from {} import'.format(app_name)
                 replace_within_file(f, dan_str, an_str)
 
-                dan_str = f'import {dist_app_name}'
-                an_str = f'import {app_name}'
+                dan_str = 'import {}'.format(dist_app_name)
+                an_str = 'import {}'.format(app_name)
                 replace_within_file(f, dan_str, an_str)
 
                 # Common trailing patterns
                 trailing = ['.', ':', '_', '/']
                 for i in trailing:
-                    dan_str = f'{dist_app_name}{i}'
-                    an_str = f'{app_name}{i}'
+                    dan_str = '{}{}'.format(dist_app_name, i)
+                    an_str = '{}{}'.format(app_name, i)
                     replace_within_file(f, dan_str, an_str)
 
                 # Common leading patterns
                 leading = ['/', 'COPY ']
                 for i in leading:
-                    dan_str = f'{i}{dist_app_name}'
-                    an_str = f'{i}{app_name}'
+                    dan_str = '{}{}'.format(i, dist_app_name)
+                    an_str = '{}{}'.format(i, app_name)
                     replace_within_file(f, dan_str, an_str)
         print()
 
@@ -126,16 +127,16 @@ def main():
     status_str = "Copying config example files:"
     print(status_str)
     for key in config_file_map.keys():
-        original_path = Path(f'{base_dir}/{key}')
-        target_path = Path(f'{base_dir}/{config_file_map[key]}')
+        original_path = Path('{}/{}'.format(base_dir, key))
+        target_path = Path('{}/{}'.format(base_dir, config_file_map[key]))
         success = copy_file(original_path, target_path)
         success_str = '  + ' if success else '  X '
         if success:
-            success_str += f'{original_path} -> {target_path}'
+            success_str += '{} -> {}'.format(original_path, target_path)
         else:
-            success_str += f'{target_path} could not be created '
-            success_str += f'(file exists or other error)'
-        print(f'{success_str}')
+            success_str += '{} could not be created '.format(target_path)
+            success_str += '(file exists or other error)'
+        print('{}'.format(success_str))
     print()
 
     # Create data and logs directories
@@ -144,18 +145,18 @@ def main():
         'logs'
     ]
 
-    status_str = f'Creating default directories:'
+    status_str = 'Creating default directories:'
     print(status_str)
     for d in new_dirs:
-        d = Path(f'{base_dir}/{d}')
+        d = Path('{}/{}'.format(base_dir, d))
         status_str = ''
         try:
             d.mkdir()
-            status_str += f'  + {d} created'
+            status_str += '  + {} created'.format(d)
         except FileExistsError as err:
-            status_str += f'  X {d} could not be created ({err})'
+            status_str += '  X {} could not be created ({})'.format(d, err)
         except Exception as e:
-            status_str += f'  X {d} could not be created ({e})'
+            status_str += '  X {} could not be created ({})'.format(d, e)
         print(status_str)
     print()
 
